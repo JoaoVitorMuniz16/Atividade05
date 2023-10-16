637b7ebe99cba913c5475640a15f7532e0dbc430
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2023-10-16 14:52:40 -0300

    Add files via upload

---
+++ joaovitor5.py
@@ -0,0 +1,10 @@
+metros = float(input("Digite o Valor em Metros \n"))
+cm = metros * 100
+mm = metros * 1000
+
+print(f"""Convertendo o valor {metros} M
+      
+      MM = {mm} mm
+      CM = {cm} cm 
+      
+      """)
